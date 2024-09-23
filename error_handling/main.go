56c5484c83fd37fc7a3081c63b46a6b294c5dc0c
Sandeep <singh29july95@gmail.com>
2024-09-23 18:04:02 +0530

    error handle code

--- error_handling/main.go
+++ error_handling/main.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
-func divOfTwoNum(a, b float64) float64 {
+func divOfTwoNum(a, b float64) (float64, error) {
 	if b == 0 {
-		return 0
+		return 0, fmt.Errorf("denominator zero nahi hona chaiya")
 	}
-	return a / b
+	return a / b, nil
 }
 
 func main() {
 	fmt.Println("Start Error Handling Code")
-	res := divOfTwoNum(10, 0)
-	fmt.Println(res)
+
+	// res, err := divOfTwoNum(4, 2)
+	// if err != nil {
+	// 	fmt.Println("Error Handling")
+	// }
+	// fmt.Println("Division of two Number is :", res)
+
+	res, _ := divOfTwoNum(10, 2) // under scope is used to avoid handing error || we have handle the error in previous code
+	fmt.Println("Division of two Number is :", res)
+
 }
